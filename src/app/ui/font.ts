46b7251eb044156d629b3691095985d791390f12
murarif <murarif64@gmail.com>
2024-09-04 22:54:58 +0700

    first commit

---
+++ src/app/ui/font.ts
@@ -0,0 +1,12 @@
+import { Lusitana, Inter } from "next/font/google";
+
+export const lusitana = Lusitana({
+  weight: ["400", "700"], // Tambahkan berat yang diinginkan
+  subsets: ["latin"],
+});
+
+
+export const inter = Inter ({
+  weight: ['400', '700'],
+  subsets: ['latin']
+})
